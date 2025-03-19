857e3ab4422227c175cfc5ae08305a9cf9d961a1
Viinicius-Muller <zandreviniciusmuller@gmail.com>
2025-03-19 18:27:49 -0300

    feat: Add key input to letters and to submit.

--- script.js
+++ script.js
@@ -1,53 +1,119 @@
 const form = document.getElementById("myForm");
 const letterBlocks = document.querySelectorAll(".letter");
 const wordInput = document.querySelector(".input");
-let blocks;
+const wordsBackground = document.querySelector(".words-bg");
 
-let tries = 0;
+let blocks;
+let curTry = 0;
 let maxTries = 3;
 let won = false;
+let compatibleLetters = "abcdefghijklmnopqrstuvwxyz";
 
+let curTypingLetter = 0;
+
+let inputWord = "";
 let rightWord = "sapao";
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  let inputWord = wordInput.value.toLowerCase();
+//Add letter typed
+window.addEventListener("keydown", function (e) {
+  const key = e.key;
 
   //Check if ended
-  if (tries >= maxTries) return;
   if (won) return;
+  if (curTry >= maxTries) return;
 
-  //Word has 5 letters?
-  if (inputWord.length !== 5) return;
+  //Submit
+  if (key === "Enter") submitTry();
 
-  //Is inside compatible words?
+  //Remove
+  if (key === "Backspace") removeLetter();
 
-  //Start
-  blocks = document.querySelectorAll(`.block-${tries}`);
-  tries++;
+  //Check typed letters
+  if (curTypingLetter >= 5) return;
 
-  //Format blocks classList
+  //Check if letter is typable
+  if (!compatibleLetters.includes(key)) return;
+
+  //Add letter to inputWord
+  inputWord += key;
+
+  //Add blocks
+  let blockLetter = blocks[curTypingLetter].querySelector(".letter");
+
+  blockLetter.textContent = key;
+
+  //Add index
+  curTypingLetter++;
+
+  console.log(inputWord);
+});
+
+function removeLetter() {
+  if (curTypingLetter <= 0) return;
+  inputWord = inputWord.slice(1);
+  curTypingLetter--;
+
+  blocks = document.querySelectorAll(`.block-${curTry}`);
+  let blockLetter = blocks[curTypingLetter].querySelector(".letter");
+  blockLetter.textContent = "";
+  console.log(inputWord);
+  return;
+}
+
+function submitTry() {
+  if (inputWord.length != 5) return;
   blocks.forEach((block, k) => {
     const letterBlock = block.querySelector(".letter");
 
     block.classList.remove("inPlace");
     block.classList.remove("contains");
 
-    //Do stuff for the letter
-    let curLetter = inputWord[k];
-    letterBlock.textContent = curLetter;
-
-    //Check if word is contained
-    if (rightWord.includes(curLetter)) {
-      //Check if word is in place
-      if (rightWord[k] == curLetter) {
+    if (rightWord.includes(letterBlock.textContent)) {
+      if (rightWord[k] === letterBlock.textContent) {
         block.classList.add("inPlace");
       } else block.classList.add("contains");
     }
   });
 
-  if (inputWord === rightWord) {
-    console.log("you win");
-    won = true;
+  //Check if right
+  if (inputWord === rightWord) won = true;
+
+  //Add try and reset stuff
+  curTry++;
+  blocks = document.querySelectorAll(`.block-${curTry}`);
+
+  inputWord = "";
+  curTypingLetter = 0;
+
+  return;
+}
+
+//Start game + configs
+function addRowBlocks(number) {
+  for (let i = 0; i < number; i++) {
+    const newBlock = `<div class="flex words-row">
+            <div class="block block-${i}"><p class="letter"></p></div>
+            <div class="block block-${i}"><p class="letter"></p></div>
+            <div class="block block-${i}"><p class="letter"></p></div>
+            <div class="block block-${i}"><p class="letter"></p></div>
+            <div class="block block-${i}"><p class="letter"></p></div>
+          </div>`;
+    wordsBackground.insertAdjacentHTML("beforeend", newBlock);
   }
-});
+}
+
+function startGame(num_of_tries) {
+  curTry = 0;
+  inputWord = "";
+  curTypingLetter = 0;
+  won = false;
+  //random word
+  rightWord = "sapao";
+
+  //Add row-blocks
+  maxTries = num_of_tries;
+  addRowBlocks(num_of_tries);
+  blocks = document.querySelectorAll(`.block-${curTry}`);
+}
+
+startGame(maxTries);
