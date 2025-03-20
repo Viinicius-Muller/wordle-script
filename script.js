c08c66a77a23469a6996dc6b6303c5b9b570d9f7
Viinicius-Muller <zandreviniciusmuller@gmail.com>
2025-03-20 10:28:08 -0300

    feat: Add 5-letters word API by cheaderthecoder. Fix removing input letters slicing wrong letters.

--- script.js
+++ script.js
@@ -3,117 +3,145 @@ const letterBlocks = document.querySelectorAll(".letter");
 const wordInput = document.querySelector(".input");
 const wordsBackground = document.querySelector(".words-bg");
 
+//Generate new word
+let word;
+let allWords;
+
+//Words API
+function getWords() {
+  const wordAPI = getJSON(
+    "https://cheaderthecoder.github.io/5-Letter-words/words.json"
+  ).then((res) => {
+    allWords = res.words;
+    rightWord = allWords[Math.floor(Math.random() * 5757)];
+  });
+}
+
 let blocks;
 let curTry = 0;
-let maxTries = 3;
+let maxTries = 6;
 let won = false;
 let compatibleLetters = "abcdefghijklmnopqrstuvwxyz";
 
 let curTypingLetter = 0;
 
 let inputWord = "";
-let rightWord = "sapao";
+let rightWord = "";
 
-//Add letter typed
-window.addEventListener("keydown", function (e) {
-  const key = e.key;
+class App {
+  constructor() {
+    window.addEventListener("keydown", this.inputsHandler.bind(this));
+  }
 
-  //Check if ended
-  if (won) return;
-  if (curTry >= maxTries) return;
+  inputsHandler(e) {
+    const key = e.key.toLowerCase();
 
-  //Submit
-  if (key === "Enter") submitTry();
+    //Check if ended
+    if (won) return;
+    if (curTry >= maxTries) return;
 
-  //Remove
-  if (key === "Backspace") removeLetter();
+    //Submit
+    if (key === "enter") this.submitTry();
+    //Remove
+    if (key === "backspace") this.removeLetter();
 
-  //Check typed letters
-  if (curTypingLetter >= 5) return;
+    //Check if letter is typable and isnt max
+    if (curTypingLetter >= 5) return;
+    if (!compatibleLetters.includes(key)) return;
 
-  //Check if letter is typable
-  if (!compatibleLetters.includes(key)) return;
+    //Add letter to inputWord
+    inputWord += key;
 
-  //Add letter to inputWord
-  inputWord += key;
+    //Add blocks
+    let blockLetter = blocks[curTypingLetter].querySelector(".letter");
+    blockLetter.textContent = key;
 
-  //Add blocks
-  let blockLetter = blocks[curTypingLetter].querySelector(".letter");
+    //Add index
+    curTypingLetter++;
+    console.log(inputWord);
+  }
 
-  blockLetter.textContent = key;
+  removeLetter() {
+    if (curTypingLetter <= 0) return;
+    inputWord = inputWord.slice(0, -1);
+    curTypingLetter--;
 
-  //Add index
-  curTypingLetter++;
+    blocks = document.querySelectorAll(`.block-${curTry}`);
+    let blockLetter = blocks[curTypingLetter].querySelector(".letter");
+    blockLetter.textContent = "";
+    console.log(inputWord);
+    return;
+  }
 
-  console.log(inputWord);
-});
+  submitTry() {
+    if (inputWord.length != 5) return;
 
-function removeLetter() {
-  if (curTypingLetter <= 0) return;
-  inputWord = inputWord.slice(1);
-  curTypingLetter--;
-
-  blocks = document.querySelectorAll(`.block-${curTry}`);
-  let blockLetter = blocks[curTypingLetter].querySelector(".letter");
-  blockLetter.textContent = "";
-  console.log(inputWord);
-  return;
-}
-
-function submitTry() {
-  if (inputWord.length != 5) return;
-  blocks.forEach((block, k) => {
-    const letterBlock = block.querySelector(".letter");
-
-    block.classList.remove("inPlace");
-    block.classList.remove("contains");
-
-    if (rightWord.includes(letterBlock.textContent)) {
-      if (rightWord[k] === letterBlock.textContent) {
-        block.classList.add("inPlace");
-      } else block.classList.add("contains");
+    if (!allWords.includes(inputWord)) {
+      console.log("Word does not exist in DataBase");
+      return;
     }
-  });
 
-  //Check if right
-  if (inputWord === rightWord) won = true;
+    blocks.forEach((block, k) => {
+      const letterBlock = block.querySelector(".letter");
 
-  //Add try and reset stuff
-  curTry++;
-  blocks = document.querySelectorAll(`.block-${curTry}`);
+      block.classList.remove("inPlace");
+      block.classList.remove("contains");
 
-  inputWord = "";
-  curTypingLetter = 0;
+      if (rightWord.includes(letterBlock.textContent)) {
+        if (rightWord[k] === letterBlock.textContent) {
+          block.classList.add("inPlace");
+        } else block.classList.add("contains");
+      }
+    });
 
-  return;
-}
+    //Check if right
+    if (inputWord === rightWord) won = true;
 
-//Start game + configs
-function addRowBlocks(number) {
-  for (let i = 0; i < number; i++) {
-    const newBlock = `<div class="flex words-row">
-            <div class="block block-${i}"><p class="letter"></p></div>
-            <div class="block block-${i}"><p class="letter"></p></div>
-            <div class="block block-${i}"><p class="letter"></p></div>
-            <div class="block block-${i}"><p class="letter"></p></div>
-            <div class="block block-${i}"><p class="letter"></p></div>
-          </div>`;
-    wordsBackground.insertAdjacentHTML("beforeend", newBlock);
+    //Add try and reset stuff
+    curTry++;
+    blocks = document.querySelectorAll(`.block-${curTry}`);
+
+    inputWord = "";
+    curTypingLetter = 0;
+
+    return;
+  }
+
+  async startGame(num_of_tries) {
+    curTry = 0;
+    inputWord = "";
+    curTypingLetter = 0;
+    won = false;
+    //random word
+    getWords();
+
+    //Add row-blocks
+    maxTries = num_of_tries;
+    this.addRowBlocks(num_of_tries);
+    blocks = document.querySelectorAll(`.block-${curTry}`);
+  }
+
+  addRowBlocks(number) {
+    for (let i = 0; i < number; i++) {
+      const newBlock = `<div class="flex words-row">
+              <div class="block block-${i}"><p class="letter"></p></div>
+              <div class="block block-${i}"><p class="letter"></p></div>
+              <div class="block block-${i}"><p class="letter"></p></div>
+              <div class="block block-${i}"><p class="letter"></p></div>
+              <div class="block block-${i}"><p class="letter"></p></div>
+            </div>`;
+      wordsBackground.insertAdjacentHTML("beforeend", newBlock);
+    }
   }
 }
 
-function startGame(num_of_tries) {
-  curTry = 0;
-  inputWord = "";
-  curTypingLetter = 0;
-  won = false;
-  //random word
-  rightWord = "sapao";
+//Start game + configs
+const app = new App();
+app.startGame(maxTries);
 
-  //Add row-blocks
-  maxTries = num_of_tries;
-  addRowBlocks(num_of_tries);
-  blocks = document.querySelectorAll(`.block-${curTry}`);
+function getJSON(url) {
+  return fetch(url).then((res) => {
+    if (!res.ok) throw new Error("API failed to load");
+    return res.json();
+  });
 }
-
-startGame(maxTries);
